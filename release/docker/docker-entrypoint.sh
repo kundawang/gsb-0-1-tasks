ad9a5ab9c9090eb00514f2e2d13a48565fedd4ed
kundawang <kundawang@users.noreply.github.com>
2026-09-26 20:02:34 +0800

    T251 base (2026-09-26)

---
+++ release/docker/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+MITMPROXY_PATH="/home/mitmproxy/.mitmproxy"
+
+if [ -f "$MITMPROXY_PATH/mitmproxy-ca.pem" ]; then
+  f="$MITMPROXY_PATH/mitmproxy-ca.pem"
+else
+  f="$MITMPROXY_PATH"
+fi
+usermod -o \
+    -u $(stat -c "%u" "$f") \
+    -g $(stat -c "%g" "$f") \
+    mitmproxy \
+    >/dev/null  # hide "usermod: no changes"
+
+if [[ "$1" = "mitmdump" || "$1" = "mitmproxy" || "$1" = "mitmweb" ]]; then
+  exec gosu mitmproxy "$@"
+else
+  exec "$@"
+fi
